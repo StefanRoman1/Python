77775bc1af6e07b0e9b5137583e6c7d61aec2b93
Stefan Roman <stefanroman1512@gmail.com>
2022-12-15 13:01:02 +0200

    check ftp function

--- Proiect/check_conectivity.py
+++ Proiect/check_conectivity.py
@@ -5,24 +5,28 @@ import psycopg2
 import pymongo
 import elasticsearch
 import warnings
+import ftplib
 warnings.filterwarnings("ignore")
 
 class check_conectivity :
     def read_data(self):
         parser = argparse.ArgumentParser()
         parser.add_argument("-url", help="url to check")
+        parser.add_argument("-ftp", help="ftp url to check")
         parser.add_argument("-mongodb", help="mongodb url to check")
         parser.add_argument("-postgresql", help="postgresql url to check")
         parser.add_argument("-elasticsearch", help="elasticSearch url to check")
         parser.add_argument("-time", help="time interval to check the connection")
         args = parser.parse_args()
         self.link = args.url or args.mongodb or args.postgresql or args.elasticsearch
-        self.type = args.url and "-url" or args.mongodb and "-mongodb" or args.postgresql and "-postgresql" or args.elasticsearch and "-elasticsearch"
+        self.type = args.url and "-url" or args.mongodb and "-mongodb" or args.postgresql and "-postgresql" or args.elasticsearch and "-elasticsearch" or args.ftp and "-ftp"
         self.x = int(args.time) if args.time else 0
 
     def check(self):
         if self.type.lower() == "-url":
             self.check_http()
+        elif self.type.lower() == "-ftp":
+            self.check_ftp()
         elif self.type.lower() == "-mongodb":
             self.check_mongo()
         elif self.type.lower() == "-postgresql":
@@ -37,6 +41,18 @@ class check_conectivity :
             print(f'Response Time: {response.elapsed.total_seconds()}')
         except:
             print(f'Connection could not be established for {self.link}')
+
+    def check_ftp(self):
+            try:
+                start = time.time()
+                ftp = ftplib.FTP(self.link)
+                ftp.login()
+                ftp.quit()
+                end = time.time()
+                print("Connection Successful")
+                print(f'Response Time: {end-start}')
+            except:
+                print(f'Connection could not be established for {self.link}')
 
     def check_postgresql(self):
             try:

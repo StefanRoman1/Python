15fd3266e0a5e22d5803e92c2dffa99c6a6ce7f1
Stefan Roman <stefanroman1512@gmail.com>
2022-12-15 17:57:04 +0200

    last updates

--- Proiect/check_conectivity.py
+++ Proiect/check_conectivity.py
@@ -6,6 +6,8 @@ import pymongo
 import elasticsearch
 import warnings
 import ftplib
+import os
+from urllib.parse import urlparse
 warnings.filterwarnings("ignore")
 
 class check_conectivity :
@@ -17,9 +19,10 @@ class check_conectivity :
         parser.add_argument("-postgresql", help="postgresql URI to check")
         parser.add_argument("-elasticsearch", help="elasticSearch URI to check")
         parser.add_argument("-time", help="time interval to check the connection in minutes")
+        parser.add_argument("-file", help="file URI to check")
         args = parser.parse_args()
-        self.link = args.url or args.mongodb or args.postgresql or args.elasticsearch or args.ftp
-        self.type = args.url and "-url" or args.mongodb and "-mongodb" or args.postgresql and "-postgresql" or args.elasticsearch and "-elasticsearch" or args.ftp and "-ftp"
+        self.link = args.url or args.mongodb or args.postgresql or args.elasticsearch or args.ftp or args.file
+        self.type = args.url and "-url" or args.mongodb and "-mongodb" or args.postgresql and "-postgresql" or args.elasticsearch and "-elasticsearch" or args.ftp and "-ftp" or args.file and "-file"
         self.x = float(args.time) if args.time else 0
 
     def check(self):
@@ -33,6 +36,8 @@ class check_conectivity :
             self.check_postgresql()
         elif self.type == "-elasticsearch":
             self.check_elasticSearch()
+        elif self.type == "-file":
+            self.check_file()
 
     def check_http(self):
         try:
@@ -64,7 +69,7 @@ class check_conectivity :
                 print("Connection Successful")
                 print(f'Response Time: {end-start}')
                 cur.close()
-            except (Exception, psycopg2.Error) as error :
+            except:
                 print (f'Connection could not be established for {self.link}')
 
     def check_mongo(self):
@@ -88,6 +93,16 @@ class check_conectivity :
             print(f'Response Time: {end-start}')
         except:
             print(f'Connection could not be established for {self.link }')
+
+    def check_file(self):
+        start = time.time()
+        p = urlparse(self.link)
+        if os.path.exists(p.path[1:]):
+            end = time.time()
+            print("Connection Successful")
+            print(f'Response Time: {end-start}')
+        else :
+            print(f'{self.link } is not a valid file')
 
 if __name__ == "__main__":
     project = check_conectivity()
